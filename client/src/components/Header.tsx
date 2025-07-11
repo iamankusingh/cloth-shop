d83864864764e5ea198a5b45023a919950eadae0
iamankusingh <ankusingh7988@gmail.com>
2025-07-11 21:54:10 +0530

    clerk authentication added

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,11 +1,27 @@
 // header of application
 
-const Header:React.FC = () => {
-  return (
-    <header className="w-screen p-2 text-2xl text-white font-semibold bg-blue-600 fixed top-0 z-10">
-      <div><h1>Cloth SHop</h1></div>
+import {
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  UserButton,
+} from "@clerk/clerk-react";
 
-      <div></div>
+const Header: React.FC = () => {
+  return (
+    <header className="w-screen p-2 flex justify-between text-2xl text-white font-semibold bg-blue-600 fixed top-0 z-10">
+      <div>
+        <h1>Cloth SHop</h1>
+      </div>
+
+      <div>
+        <SignedOut>
+          <SignInButton />
+        </SignedOut>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
+      </div>
     </header>
   );
 };
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -6,17 +6,26 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import App from "./App.tsx";
 import Customizer from "./pages/Customizer.tsx";
 import CanvasModel from "./canvas/index.tsx";
+import { ClerkProvider } from "@clerk/clerk-react";
+
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error("Missing Publishable Key");
+}
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
-      {/* canvas model is here to prevent it from unmounting and re-mounting  */}
-      <CanvasModel />
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
+      <BrowserRouter>
+        {/* canvas model is here to prevent it from unmounting and re-mounting  */}
+        <CanvasModel />
 
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/customizer" element={<Customizer />} />
-      </Routes>
-    </BrowserRouter>
+        <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="/customizer" element={<Customizer />} />
+        </Routes>
+      </BrowserRouter>
+    </ClerkProvider>
   </StrictMode>
 );
